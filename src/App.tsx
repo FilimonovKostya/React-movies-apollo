a6dee0f867fdadcdd582db5e114f35322f2d9422
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-03-14 23:43:03 +0400

    was added appBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  InputBase,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Weather
+          </Typography>
+          <Paper>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search Google Maps"
+              inputProps={{ "aria-label": "search google maps" }}
+            />
+            <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
