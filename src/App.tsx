16770470bd131148fffd318c7ac3d913b704eecb
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-03-15 11:08:03 +0400

    was created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
 import React from "react";
-import {
-  AppBar,
-  Box,
-  IconButton,
-  InputBase,
-  Paper,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import { Box, Paper, Typography } from "@mui/material";
+import { indigo } from "@mui/material/colors";
 
 function App() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            Weather
-          </Typography>
-          <Paper>
-            <InputBase
-              sx={{ ml: 1, flex: 1 }}
-              placeholder="Search Google Maps"
-              inputProps={{ "aria-label": "search google maps" }}
-            />
-            <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-              <SearchIcon />
-            </IconButton>
-          </Paper>
-        </Toolbar>
-      </AppBar>
+    <Box sx={{ width: "100%" }}>
+      <Paper
+        variant="elevation"
+        elevation={5}
+        square
+        sx={{ width: "100%", background: indigo["900"] }}
+      >
+        <Typography variant="h1" color="white" textAlign="center">
+          Movies Hub
+        </Typography>
+      </Paper>
     </Box>
   );
 }
