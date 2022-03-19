f10175771a34b10933926cb30659df83d6960c8a
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-03-19 21:31:26 +0400

    was fixed styles in swiper(intermediate)

--- src/components/SwiperComponent.tsx
+++ src/components/SwiperComponent.tsx
@@ -1,38 +1,68 @@
 import React from "react";
-
-// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line import/extensions,import/no-unresolved
 import { Swiper, SwiperSlide } from "swiper/react";
 // eslint-disable-next-line import/no-unresolved
 import "swiper/css";
-// eslint-disable-next-line import/no-unresolved
-import "swiper/css/navigation";
-
-import { Autoplay, Navigation } from "swiper";
+import { Autoplay } from "swiper";
+import "../index.css";
 
 function SwiperComponent() {
   return (
     <Swiper
-      spaceBetween={30}
-      centeredSlides
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      modules={[Autoplay, Navigation]}
+      className="swiper swiper-slide"
+      slidesPerView={4}
+      loop
+      // autoplay={{
+      //   delay: 1000,
+      //   disableOnInteraction: false,
+      // }}
       pagination={{
         clickable: true,
       }}
-      navigation
+      modules={[Autoplay]}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://image.tmdb.org/t/p/w300//aTcqu8cI4wMohU17xTdqmXKTGrw.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://image.tmdb.org/t/p/w300//aTcqu8cI4wMohU17xTdqmXKTGrw.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://image.tmdb.org/t/p/w300//aTcqu8cI4wMohU17xTdqmXKTGrw.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://image.tmdb.org/t/p/w300//aTcqu8cI4wMohU17xTdqmXKTGrw.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://image.tmdb.org/t/p/w300//aTcqu8cI4wMohU17xTdqmXKTGrw.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://image.tmdb.org/t/p/w300//aTcqu8cI4wMohU17xTdqmXKTGrw.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://image.tmdb.org/t/p/w300//aTcqu8cI4wMohU17xTdqmXKTGrw.jpg"
+          alt=""
+        />
+      </SwiperSlide>
     </Swiper>
   );
 }
